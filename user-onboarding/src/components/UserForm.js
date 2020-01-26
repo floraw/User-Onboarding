82da7b6ef9ad0c4a7dcb739fddcedb3949b5fa55
floraw <flora.e.winters@gmail.com>
2020-01-26 14:07:01 -0800

    MVP

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import User from './User';
 
 const UserForm = ({ errors, touched, values, status }) => {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
         status && setUsers(users => [...users, status]);
+        console.log(status);
     }, [status]);
 
     return (
@@ -39,22 +39,24 @@ const UserForm = ({ errors, touched, values, status }) => {
                     />
                     {touched.password && errors.password && <p>{errors.password}</p>}
 
-                    <label htmlFor="tos">Terms of Service</label>
-                    <Field 
-                        type="checkbox"
-                        name="tos"
-                        value={values.tos}
-                    />
+                    <label htmlFor="tos">Terms of Service
+                        <Field 
+                            type="checkbox"
+                            name="tos"
+                            checked={values.tos}
+                            value={values.tos}
+                        />
+                    </label>
 
                     <button type="submit">Submit</button>
                 </Form>
             </div>
             <div className='users'>
                 {users.map(user => (
-                    <User
-                        name={user.name}
-                        email={user.email}
-                    />
+                    <div className="user-card"> 
+                        <h3>{user.name}</h3>
+                        <p>{user.email}</p>
+                    </div>
                 ))}
             </div>
         </div>
@@ -72,17 +74,27 @@ const FormikForm = withFormik({
     },
 
     validationScheme: Yup.object().shape({
-        name: Yup.string().required("Please enter a name"),
-        email: Yup.string().required("Please enter an email address"),
-        password: Yup.string().required("Please enter a password"),
+        name: Yup.string()
+        .min(2, "Please enter a name")
+        .required("Please enter a name"),
+        
+        email: Yup.string()
+        .email("Please enter a valid email address")
+        .required("Please enter an email address"),
+        
+        password: Yup.string()
+        .min(8, "Password must be 8+ characters.")
+        .required("Please enter a password"),
+        
         tos: Yup.bool()
+        .required("You must agree to the terms of service to continue")
     }),
 
-    handleSubmit(values, { setStatus, resetForm}) {
+    handleSubmit(values, { setStatus, resetForm }) {
         console.log("Submitting form: ", values);
 
         axios
-            .post("https://reqres.in/api/users")
+            .post("https://reqres.in/api/users", values)
             .then(response => {
                 console.log("Success: ", response);
                 setStatus(response.data);
